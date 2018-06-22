39293dda088862d2841ea1bac105f9d112193faf
Harvin Llanes <harvin.llanes@gmail.com>
2018-06-22 00:39:19 -0600

    Add minor refs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,16 @@
 class HomeController < ApplicationController
+  def index
+  end
+
+  def locations
+    points = [
+      { lat: 25.774, lng: -80.19 },
+      { lat: 18.466, lng: -66.118 },
+      { lat: 32.321, lng: -64.757 }
+    ]
+    render json: {
+      success: true,
+      points: points
+    },status: :ok
+  end
 end
